e3e62973c25c6177aa4c9e5e69edb04cc62e7a2e
JasonTK421 <jasontk421@gmail.com>
2019-04-09 17:45:05 -0700

    Got number buttons to populate display field when clicked

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField] Button button1;
+
+    float num1;
+    float num2;
+
+    public void SetNum1(float tempNum1) { num1 = tempNum1; }
+    public void SetNum2(float tempNum2) { num2 = tempNum2; }
+}
---
+++ Assets/Scripts/MyButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MyButton : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] TMP_Text text;
+
+    [SerializeField] float num;
+
+    void Start()
+    {
+        if (!text)
+        {
+            Debug.Log("Please assign an Text to: " + name);
+        }
+        else
+        {
+            text.text = num.ToString();
+        }
+    }
+
+    public void AddButtonNumToInputField()
+    {
+        if (!inputField)
+        {
+            Debug.Log("Please assign an inputField to: " + name);
+        }
+        else
+        {
+            inputField.text += num;
+        }
+    }
+}
