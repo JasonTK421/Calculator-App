8a015492c1d22b89e90cb0b5c011fe42369fb796
JasonTK421 <jasontk421@gmail.com>
2019-04-19 17:41:30 -0700

    Converting display field to an infix array, started working on converting infix array to postfix stack

--- Assets/Scripts/MyController.cs
+++ Assets/Scripts/MyController.cs
@@ -12,11 +12,70 @@ public class MyController : MonoBehaviour
 
     string lastKeyPressed;
 
+    void Start()
+    {
+        SolveProblem();
+    }
+
     public void EqualButton()
     {
         float answer = Evaluate(displayField.text);
 
         displayField.text = answer.ToString();
+    }
+
+    void SolveProblem()
+    {
+        CreatePostfixArray(CreateInfixArray("6+5*3-(21/6.54)"));     
+    }
+
+    ArrayList CreateInfixArray(string displayText)
+    {
+        ArrayList infix = new ArrayList();
+        string temp = "";
+        char lastPosition = '0';
+
+        for (int i = 0; i < displayText.Length; i++)
+        {
+            char currentPosition = displayText[i];
+
+            if (!IsOperand(currentPosition)) { temp += currentPosition; }
+            else if (IsOperand(lastPosition)) { infix.Add(currentPosition); }
+            else
+            {
+                infix.Add(temp);
+                temp = "";
+                infix.Add(currentPosition);
+            }
+            lastPosition = currentPosition;
+        }
+        return infix;
+    }
+
+    bool IsOperand(char character)
+    {
+        if (character == '(' || character == ')' || character == '*' ||
+            character == '/' || character == '+' || character == '-')
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void CreatePostfixArray(ArrayList infix)
+    {
+        Stack postfix = new Stack();
+        Stack TempStack = new Stack();
+
+        for (int i = 0; i < infix.Count; i++)
+        {
+            if ((string)infix[i] == "(" || (string)infix[i] == ")" ||
+                (string)infix[i] == "*" || (string)infix[i] == "/" ||
+                (string)infix[i] == "+" || (string)infix[i] == "-")
+            {
+                Debug.Log(infix[i - 1]);
+            }
+        }
     }
 
     static float Evaluate(string expression)
