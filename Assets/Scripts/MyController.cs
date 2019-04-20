7ef453e458b423f123143007179b3a5731df31bf
JasonTK421 <jasontk421@gmail.com>
2019-04-19 19:37:31 -0700

    Finished creating postfix ArrayList

--- Assets/Scripts/MyController.cs
+++ Assets/Scripts/MyController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class MyController : MonoBehaviour
 {
     static string ZERO = "0";
-
+    
     [SerializeField] TMP_InputField displayField;
 
     string lastKeyPressed;
@@ -29,18 +30,93 @@ public class MyController : MonoBehaviour
         CreatePostfixArray(CreateInfixArray("6+5*3-(21/6.54)"));     
     }
 
+    void CreatePostfixArray(ArrayList infix)
+    {
+        ArrayList postfix = new ArrayList();
+        Stack tempStack = new Stack();
+
+        foreach(string item in infix)
+        {
+            if (!IsOperator(item))
+            {
+                postfix.Add(item);
+            }
+            else
+            {
+                switch (item)
+                {
+                    case "(":
+                        tempStack.Push(item);
+                        break;
+                    case ")":
+                        while(tempStack.Peek().ToString() != "(")
+                        {
+                            postfix.Add(tempStack.Pop());
+                        }
+                        tempStack.Pop();
+                        break;
+                    default:
+                        while(tempStack.Count > 0 && 
+                            tempStack.Peek().ToString() != "(" && 
+                            !HasPrecedence(item, tempStack.Peek().ToString()))
+                        {
+                            postfix.Add(tempStack.Pop());
+                        }
+                        tempStack.Push(item);
+                        break;
+                }
+            }
+        }
+        while(tempStack.Count > 0)
+        {
+            postfix.Add(tempStack.Pop());
+        }
+
+        foreach(string item in postfix)
+        {
+            Debug.Log(item);
+        }
+    }
+
+    bool HasPrecedence(string character, string stack)
+    {
+        int a = AssignValue(character);
+        int b = AssignValue(stack);
+
+        if (a > b) { return true; }
+        else { return false; }
+    }
+
+    int AssignValue(string myOperator)
+    {
+
+        switch (myOperator)
+        {
+            case "-":
+                return 0;
+            case "+":
+                return 1;
+            case "/":
+                return 2;
+            case "*":
+                return 3;
+            default:
+                return 0;
+        }
+    }
+
     ArrayList CreateInfixArray(string displayText)
     {
         ArrayList infix = new ArrayList();
         string temp = "";
-        char lastPosition = '0';
+        string lastPosition = "";
 
         for (int i = 0; i < displayText.Length; i++)
         {
-            char currentPosition = displayText[i];
+            string currentPosition = displayText[i].ToString();
 
-            if (!IsOperand(currentPosition)) { temp += currentPosition; }
-            else if (IsOperand(lastPosition)) { infix.Add(currentPosition); }
+            if (!IsOperator(currentPosition)) { temp += currentPosition; }
+            else if (IsOperator(lastPosition)) { infix.Add(currentPosition); }
             else
             {
                 infix.Add(temp);
@@ -52,20 +128,14 @@ public class MyController : MonoBehaviour
         return infix;
     }
 
-    bool IsOperand(char character)
+    bool IsOperator(string character)
     {
-        if (character == '(' || character == ')' || character == '*' ||
-            character == '/' || character == '+' || character == '-')
+        if (character == "(" || character == ")" || character == "*" ||
+            character == "/" || character == "+" || character == "-")
         {
             return true;
         }
         return false;
-    }
-
-    void CreatePostfixArray(ArrayList infix)
-    {
-        Stack postfix = new Stack();
-        Stack TempStack = new Stack();
     }
 
     static float Evaluate(string expression)
